fdbcb6cd0d25d1101c559d47515ee1e986faf714
Daniel Elkington <20923693+danielelkington@users.noreply.github.com>
2021-02-07 21:48:57 +1100

    [DOCS] Add Vitepress docs

---
+++ vue-camera-gestures/docs/components/load-mobile-net.vue
@@ -0,0 +1,9 @@
+<!--Doesn't do much - just kicks off loading mobilenet for better performance-->
+<script>
+import { loadMobilenet } from "../../dist/vue-camera-gestures.esm.js"
+export default {
+  created () {
+    loadMobilenet()
+  }
+}
+</script>
